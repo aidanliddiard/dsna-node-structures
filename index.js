25b2c7593d84f58c3d8a2637656d417f8d69bd84
aidanliddiard <aidanliddiard@gmail.com>
2022-06-28 12:28:04 -0700

    added julie answer

--- index.js
+++ index.js
@@ -46,65 +46,84 @@ class BinaryList {
     this.root = new BinaryTreeNode(value);
   }
 
-  add(node) {
-    let object = this.root;
-    let current = this.root.value;
-    console.log("current", current);
-    console.log("node", node);
-    while (current != null) {
-      if (node < current && object.left === null) {
-        object.left = node;
-        current = null;
-      } else if (node < current && object.left != null) {
-        current = current.left;
-      } else if (node > current && current.right === null) {
-        current.right = node;
-        current = null;
-      } else if (node > current && current.right != null) {
-        current = current.right;
+  insert(value) {
+    const newNode = new BinaryTreeNode(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(current, newNode) {
+    if (newNode.value < current.value) {
+      if (current.left === null) {
+        current.left = newNode;
+      } else {
+        this.insertNode(current.left, newNode);
+      }
+    } else {
+      if (newNode.value > current.value) {
+        if (current.right === null) {
+          current.right = newNode;
+        } else {
+          this.insertNode(current.right, newNode);
+        }
       }
     }
-    // } else {
-    //   while (node > current) {
-    //     if (current.right === null) {
-    //       current.right = node;
-    //     } else current = current.right;
-    //   }
-    // }
-
-    // node to root
-    //node to current and left/right
-    //if current.left or right is null add node
-
-    // let current = this.head;
-    // while (current.right) {
-    //   current.left = current;
-    //   current = current.right;
-    // }
-    // current.right = new BinaryList(value);
   }
 }
+// OUR ATTEMPT
+// add(node) {
+//   let object = this.root;
+//   let current = this.root.value;
+//   console.log("current", current);
+//   console.log("node", node);
+//   while (current != null) {
+//     if (node < current && object.left === null) {
+//       object.left = node;
+//       current = null;
+//     } else if (node < current && object.left != null) {
+//       current = current.left;
+//     } else if (node > current && current.right === null) {
+//       current.right = node;
+//       current = null;
+//     } else if (node > current && current.right != null) {
+//       current = current.right;
+//     }
+//   }
+
+// } else {
+//   while (node > current) {
+//     if (current.right === null) {
+//       current.right = node;
+//     } else current = current.right;
+//   }
+// }
+
+// node to root
+//node to current and left/right
+//if current.left or right is null add node
+
+// let current = this.head;
+// while (current.right) {
+//   current.left = current;
+//   current = current.right;
+// }
+// current.right = new BinaryList(value);
+
 // console.log('head', this.head);
 // console.log('right', current.right)
 // console.log('left', current.left)
 const C = new BinaryList("C");
-const A = new BinaryList("A");
+const A = new BinaryTreeNode("A");
 
 console.log("C", C);
-C.add("F");
+C.insert("A");
 console.log("f", C);
-C.add("B");
+C.insert("B");
 console.log("b", C);
 
-// C.add("F");
-// console.log("F", C);
-// const B = new BinaryList("B");
-// const A = new BinaryList("A");
-// const C = new BinaryList("C");
-// const D = new BinaryList("D");
-
-// // B will be the root of the tree:
-// B.add(A);
-// B.add(D);
-// B.add(C);
-// console.log("b", B);
+C.insert("F");
+console.log("F", C);
