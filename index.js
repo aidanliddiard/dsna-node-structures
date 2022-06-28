7c30f88abe1c633a1d9dd7e01b260b74d9058159
Khayman King <khaymanak@gmail.com>
2022-06-28 10:32:11 -0700

    not working

--- index.js
+++ index.js
@@ -27,11 +27,45 @@ class LinkedList {
   }
 }
 
-const list = new LinkedList("A");
-console.log(list.head);
-list.add("B");
-console.log(list.getList());
-list.add("C");
-console.log(list.getList());
+// const list = new LinkedList("A");
+// console.log(list.head);
+// list.add("B");
+// console.log(list.getList());
+// list.add("C");
+// console.log(list.getList());
 
-module.exports = { LinkedList };
+// module.exports = { LinkedList };
+
+
+
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinaryList {
+  constructor(value) {  
+    this.head = new BinaryTreeNode(value)
+  }
+
+  add(node) {
+    let current = this.head;
+    while(current.right){
+      current.left = current
+      current = current.right
+    }
+    current.right = new BinaryList(value)
+  }
+}
+// console.log('head', this.head);
+// console.log('right', current.right)
+// console.log('left', current.left)
+
+const B = new BinaryList('B');
+const A = new BinaryList('A');
+console.log('b', B.head)
+console.log('A', current.left.add('A'))
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
