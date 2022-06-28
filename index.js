7dd775a00484916d77c83042737057ba0ceb0f3f
aidanliddiard <aidanliddiard@gmail.com>
2022-06-28 12:12:34 -0700

    not working still

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ class LinkedList {
     this.head = new Node(val);
   }
 
-  add(){
-
-  }
-  removeTail(){}
+  add() {}
+  removeTail() {}
 
   getList() {
     const listArr = [];
@@ -36,8 +34,6 @@ class LinkedList {
 
 // module.exports = { LinkedList };
 
-
-
 class BinaryTreeNode {
   constructor(value) {
     this.value = value;
@@ -46,26 +42,69 @@ class BinaryTreeNode {
   }
 }
 class BinaryList {
-  constructor(value) {  
-    this.head = new BinaryTreeNode(value)
+  constructor(value) {
+    this.root = new BinaryTreeNode(value);
   }
 
   add(node) {
-    let current = this.head;
-    while(current.right){
-      current.left = current
-      current = current.right
+    let object = this.root;
+    let current = this.root.value;
+    console.log("current", current);
+    console.log("node", node);
+    while (current != null) {
+      if (node < current && object.left === null) {
+        object.left = node;
+        current = null;
+      } else if (node < current && object.left != null) {
+        current = current.left;
+      } else if (node > current && current.right === null) {
+        current.right = node;
+        current = null;
+      } else if (node > current && current.right != null) {
+        current = current.right;
+      }
     }
-    current.right = new BinaryList(value)
+    // } else {
+    //   while (node > current) {
+    //     if (current.right === null) {
+    //       current.right = node;
+    //     } else current = current.right;
+    //   }
+    // }
+
+    // node to root
+    //node to current and left/right
+    //if current.left or right is null add node
+
+    // let current = this.head;
+    // while (current.right) {
+    //   current.left = current;
+    //   current = current.right;
+    // }
+    // current.right = new BinaryList(value);
   }
 }
 // console.log('head', this.head);
 // console.log('right', current.right)
 // console.log('left', current.left)
+const C = new BinaryList("C");
+const A = new BinaryList("A");
 
-const B = new BinaryList('B');
-const A = new BinaryList('A');
-console.log('b', B.head)
-console.log('A', current.left.add('A'))
-const C = new BinaryTreeNode('C');
-const D = new BinaryTreeNode('D');
+console.log("C", C);
+C.add("F");
+console.log("f", C);
+C.add("B");
+console.log("b", C);
+
+// C.add("F");
+// console.log("F", C);
+// const B = new BinaryList("B");
+// const A = new BinaryList("A");
+// const C = new BinaryList("C");
+// const D = new BinaryList("D");
+
+// // B will be the root of the tree:
+// B.add(A);
+// B.add(D);
+// B.add(C);
+// console.log("b", B);
